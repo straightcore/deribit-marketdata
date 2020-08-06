fc00e94f7f901d80e8342b297eb8ccef7720df63
Morgan Dupire <dupire.morgan@gmail.com>
2020-08-07 01:04:50 +0200

    [marketData] add intervale in configuration for websocket

--- src/Server/MarketData.Adapter.Deribit.Host/Configuration/ServiceConfig.cs
+++ src/Server/MarketData.Adapter.Deribit.Host/Configuration/ServiceConfig.cs
@@ -6,7 +6,13 @@ namespace MarketDataAdapterService.Configuration
     {
         public string Uri { get; set; }
         public Authentification Authentification { get; set; }
-
+        /// <summary>
+        /// Gets or sets frequency of notifications. 
+        /// Events will be aggregated over this interval. 
+        /// The value raw means no aggregation will be applied.
+        /// e.g: 100ms, 1000ms
+        /// </summary>
+        public string FetchInterval {get;set;}
         public bool TestMode { get; set; }
     }
 }
